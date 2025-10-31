7c0ed4276beffb20628b2f1013e52a77f1cb01f7
carsons11 <carsons11@nycstudents.net>
2025-10-31 09:26:37 -0400

    Update class.py

--- class.py
+++ class.py
@@ -1,4 +1,27 @@
-""" class Hero:
+# class Hero:
+#     def __init__(self, name, money, inventory):
+#         self.name = name
+#         self.money = money
+#         self.inventory = inventory
+
+#     def buy(self, item):
+#         self.inventory.append(item)
+#         print(self.inventory)
+# Jillian = Hero("Jillian", 150, ["Potion"])
+# Jillian.buy({"title": "Sword", "atk": 34})
+# print(Jillian.__dict__) 
+
+# class BankAccount:
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.__balance = balance  # double underscore means "private"
+#     def deposit(self, amount):
+#         self.__balance += amount
+#     def show_balance(self):
+#         print(f"{self.owner} has ${self.__balance}")
+
+
+class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -6,18 +29,11 @@
 
     def buy(self, item):
         self.inventory.append(item)
-        print(self.inventory)
-Jillian = Hero("Jillian", 150, ["Potion"])
-Jillian.buy({"title": "Sword", "atk": 34})
-print(Jillian.__dict__) """
+        print (f"This is your inventory {self.inventory}")
+Nathan = Hero("Batman", 200, ['Batarang'])
+print (Nathan.__dict__)
+Nathan.buy({"title": "Batmobile", "maxspeed": "Mach 1"})
 
-class BankAccount:
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.__balance = balance  # double underscore means "private"
 
-    def deposit(self, amount):
-        self.__balance += amount
-
-    def show_balance(self):
-        print(f"{self.owner} has ${self.__balance}")
+class Pet:
+    def 
