8e71faaed2e9085647ca7a1bb44a57476c090435
carsons11 <carsons11@nycstudents.net>
2025-10-31 08:50:55 -0400

    Update class.py

--- class.py
+++ class.py
@@ -1,4 +1,4 @@
-class Hero:
+""" class Hero:
     def __init__(self, name, money, inventory):
         self.name = name
         self.money = money
@@ -9,4 +9,15 @@ class Hero:
         print(self.inventory)
 Jillian = Hero("Jillian", 150, ["Potion"])
 Jillian.buy({"title": "Sword", "atk": 34})
-print(Jillian.__dict__)
+print(Jillian.__dict__) """
+
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance  # double underscore means "private"
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def show_balance(self):
+        print(f"{self.owner} has ${self.__balance}")
