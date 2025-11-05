f30b7fd98942c73004a8f5e45edf9a3ddcf816e7
carsons11 <carsons11@nycstudents.net>
2025-11-05 09:26:51 -0500

    Update class.py

--- class.py
+++ class.py
@@ -35,28 +35,26 @@
 # Nathan.buy({"title": "Batmobile", "maxspeed": "Mach 1"})
 
 
-class Pet:
-    def __init__(self,name,animal):
-        self.__name__ = name
-        self.__animal__ = animal
-    def play(self,happinesslevel):
-        x = input("What game is your pet playing?")
-        y = int(input("How much happiness is your pet gaining?"))
-        happinesslevel += y
-        print (f"After playing {x}, this is your pet's happiness: {happinesslevel}.")
-Cat=Pet("Meow","cat")
-Cat.play(5)
+# class Pet:
+#     def __init__(self,name,animal):
+#         self.__name__ = name
+#         self.__animal__ = animal
+#     def play(self,happinesslevel):
+#         x = input("What game is your pet playing?")
+#         y = int(input("How much happiness is your pet gaining?"))
+#         happinesslevel += y
+#         print (f"After playing {x}, this is your pet's happiness: {happinesslevel}.")
+# Cat=Pet("Meow","cat")
+# Cat.play(5)
 
 
 class Hero:
-    def __init__(self, name, money, inventory):
+    def __init__(self, name, money, inventory, cost):
         self.name = name
-        self.money = money
+        self.__money = money
         self.inventory = inventory
-
+        self.cost = cost
     def buy(self, item):
         self.inventory.append(item)
         print(self.inventory)
 Jillian = Hero("Jillian", 150, ["Potion"])
-Jillian.buy({"title": "Sword", "atk": 34})
-print(Jillian.__dict__)
